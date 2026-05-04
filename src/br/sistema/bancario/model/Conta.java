1af658669fc04df83b155a3f6f38c9217cd9d8d1
Gabriel Ribeiro Barbosa da Silva <gabriel.ribeiro.099@ufrn.edu.br>
2026-05-04 19:30:19 -0300

    feat: create conta (#3)

--- src/br/sistema/bancario/model/Conta.java
+++ src/br/sistema/bancario/model/Conta.java
@@ -2,8 +2,22 @@ package br.sistema.bancario.model;
 
 public class Conta {
     private String numero;
+    private double saldo;
+
+    public Conta(String numero) {
+        this.numero = numero;
+        this.saldo = 0.0;
+    }
 
     public String getNumero() {
         return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 }
--- src/br/sistema/bancario/view/ConsoleView.java
+++ src/br/sistema/bancario/view/ConsoleView.java
@@ -2,12 +2,51 @@ package br.sistema.bancario.view;
 
 import br.sistema.bancario.controller.ContaController;
 
+import java.util.Scanner;
+
 public class ConsoleView {
     private ContaController controller;
+    private Scanner scanner;
 
     public ConsoleView(ContaController controller) {
         this.controller = controller;
+        this.scanner = new Scanner(System.in);
     }
 
-    public void iniciar() {}
+    public void iniciar() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("\n=== SISTEMA BANCÁRIO ===");
+            System.out.println("1. Cadastrar Conta");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            if (scanner.hasNextInt()) {
+                opcao = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (opcao) {
+                    case 1:
+                        cadastrarContaView();
+                        break;
+                    case 0:
+                        System.out.println("Saindo do sistema...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida! Tente novamente.");
+                }
+            } else {
+                System.out.println("Entrada inválida! Digite um número.");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private void cadastrarContaView() {
+        System.out.print("Digite o número da nova conta: ");
+        String numero = scanner.nextLine();
+
+        String mensagem = controller.cadastrarConta(numero);
+        System.out.println(mensagem);
+    }
 }
